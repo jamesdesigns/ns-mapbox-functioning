485d90a690544b50d0b4f31a90adced7a271a3d2
James Hooper <jameshooperdesigns@gmail.com>
2019-06-17 15:55:46 -0600

    Add favorites page to the UTA app

--- app/app.js
+++ app/app.js
@@ -6,5 +6,7 @@ application.run({ moduleName: "app-root" });
 Do not place any code after the application has been started as it will not
 be executed on iOS.
 */
-import { Fontawesome } from 'nativescript-fontawesome';
-Fontawesome.init();
+
+
+// import { Fontawesome } from 'nativescript-fontawesome';
+// Fontawesome.init();
