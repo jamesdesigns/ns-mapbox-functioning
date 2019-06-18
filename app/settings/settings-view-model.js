fb14dcf3d191be084b39e35fbfe437f4138aa867
James Hooper <jameshooperdesigns@gmail.com>
2019-06-18 15:34:18 -0600

    Add a list of settings categories

--- app/settings/settings-view-model.js
+++ app/settings/settings-view-model.js
@@ -3,6 +3,24 @@ const observableModule = require("tns-core-modules/data/observable");
 function SettingsViewModel() {
     const viewModel = observableModule.fromObject({
         /* Add your view model properties here */
+        items: [
+            {
+                name: "Delays",
+                description: "List of Current Delays"
+            },
+            {
+                name: "Privacy",
+                description: "UTA Motions Privacy Policy"
+            },
+            {
+                name: "Alerts",
+                description: "UTA Frontrunner Alerts"
+            },
+            {
+                name: "Feedback",
+                description: "We Want Your Feedback"
+            }
+        ]
     });
 
     return viewModel;
