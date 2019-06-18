6a05df69e9b0f323a9fe63401a0583f5f96fb810
James Hooper <jameshooperdesigns@gmail.com>
2019-06-18 17:34:02 -0600

    Edit the wording of the onboarding

--- app/welcome/slides/slide1.js
+++ app/welcome/slides/slide1.js
@@ -4,7 +4,7 @@ let slide = `
 		<Label class="lobster-regular carousel-item-head" text="Welcome to" textWrap="true"></Label>
 	</GridLayout>
 	<GridLayout class="carousel-item-circle" row="1" horizontalAlignment="center" verticalAlignment="center">
-		<Label class="fa carousel-item-icon" text="&#xf19c;" textWrap="true"></Label>
+		<Label class="fa carousel-item-icon" text="&#xf041;" textWrap="true"></Label>
 	</GridLayout>
 	<GridLayout width="49%" row="2" horizontalAlignment="center" verticalAlignment="center">
 		<Label class="opensans-regular carousel-item-desc" text="Let's take a quick overview of all our features." textWrap="true"></Label>
