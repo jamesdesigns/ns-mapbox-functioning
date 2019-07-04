8074ba7dc329ff18dbccd1e886aeeab6fc57e01a
James Hooper <jameshooperdesigns@gmail.com>
2019-07-04 00:49:06 -0600

    Make changes to the app

---
+++ app/Test/test-view-model.js
@@ -0,0 +1,33 @@
+const observableModule = require("tns-core-modules/data/observable");
+const topmost = require("tns-core-modules/ui/frame").topmost;
+
+function TestViewModel() {
+  const viewModel = observableModule.fromObject({
+    showModal() {
+      const page = topmost().currentPage;
+      page.showModal(
+        "./modal/modal", // Path to the xml file of modal without extension
+        { // Pass any context you want to use in the modal
+          context: "Some data",
+          foodType: "Veg",
+          food: [
+            {
+              name: "Carrot"
+            },
+            {
+              name: "Potatoe"
+            }
+          ]
+        },
+        function closeCallback(result) { // you can customise this callback the way you want
+          console.log("Result was: ", result);
+        },
+        false // Full screen or not? (on iOS the modal is fullscreen irrespective of this value)
+      );
+    }
+  });
+
+  return viewModel;
+}
+
+module.exports = TestViewModel;
--- app/routes/routes-items-page.js
+++ app/routes/routes-items-page.js
@@ -59,40 +59,40 @@ exports.showModal = function (args) {
 
 
 
-// This is for the Modal implementation
-exports.pageLoaded = function(args) {
-    const page = args.object;
-    page.bindingContext = RoutesItemsViewModel;
-  }
+// // This is for the Modal implementation
+// exports.pageLoaded = function(args) {
+//     const page = args.object;
+//     page.bindingContext = RoutesItemsViewModel;
+//   }
 
-exports.showModal = function (args) {
-    const page = args.object.page;
-    page.showModal(
-      "./modal/modal", // Path to the xml file of modal without extension
-      { // Pass any context you want to use in the modal
-        // you can pass anything you want as the context, even a viewModel!
-        context: "Some data",
-        foodType: "Veg",
-        food: [
-          {
-            name: "Carrot"
-          },
-          {
-            name: "Potatoe"
-          }
-        ]
-      },
-      function closeCallback(result) { // you can customise this callback the way you want
-        // if modal was closed without submitting, this will still get called
-        // so check if the parameters are defined before you use them
-        if (result) {
-          console.log("Result was: ", result);
-        }
-      },
-      false // Full screen or not? (on iOS the modal is fullscreen irrespective of this value)
-    );
-  }
- // End of the Modal Implementation 
+// exports.showModal = function (args) {
+//     const page = args.object.page;
+//     page.showModal(
+//       "./modal/modal", // Path to the xml file of modal without extension
+//       { // Pass any context you want to use in the modal
+//         // you can pass anything you want as the context, even a viewModel!
+//         context: "Some data",
+//         foodType: "Veg",
+//         food: [
+//           {
+//             name: "Carrot"
+//           },
+//           {
+//             name: "Potatoe"
+//           }
+//         ]
+//       },
+//       function closeCallback(result) { // you can customise this callback the way you want
+//         // if modal was closed without submitting, this will still get called
+//         // so check if the parameters are defined before you use them
+//         if (result) {
+//           console.log("Result was: ", result);
+//         }
+//       },
+//       false // Full screen or not? (on iOS the modal is fullscreen irrespective of this value)
+//     );
+//   }
+//  // End of the Modal Implementation 
 
 
 
