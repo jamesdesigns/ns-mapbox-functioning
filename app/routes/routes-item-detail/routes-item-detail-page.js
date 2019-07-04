11763a6cbb13f946598604c28f6d3a82ad49ce47
James Hooper <jameshooperdesigns@gmail.com>
2019-07-04 00:38:18 -0600

    Make changes by deleting the original code

--- app/routes/routes-item-detail/routes-item-detail-page.js
+++ app/routes/routes-item-detail/routes-item-detail-page.js
@@ -1,4 +1,6 @@
 
+
+
 //
 //
 // Beginning of Original
@@ -38,31 +40,3 @@ exports.onBackButtonTap = onBackButtonTap; // Original
 // End of Original
 //
 //
-
-exports.showModal = function (args) {
-    const page = args.object.page;
-    page.showModal(
-      "./modal/modal", // Path to the xml file of modal without extension
-      { // Pass any context you want to use in the modal
-        // you can pass anything you want as the context, even a viewModel!
-        context: "Some data",
-        foodType: "Veg",
-        food: [
-          {
-            name: "Carrot"
-          },
-          {
-            name: "Potatoe"
-          }
-        ]
-      },
-      function closeCallback(result) { // you can customise this callback the way you want
-        // if modal was closed without submitting, this will still get called
-        // so check if the parameters are defined before you use them
-        if (result) {
-          console.log("Result was: ", result);
-        }
-      },
-      false // Full screen or not? (on iOS the modal is fullscreen irrespective of this value)
-    );
-  }
