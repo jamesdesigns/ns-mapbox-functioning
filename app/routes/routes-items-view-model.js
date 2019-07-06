cef432157d58a0eee12fa1b139557734fe4bf3da
James Hooper <jameshooperdesigns@gmail.com>
2019-07-06 14:21:49 -0600

    Add a fixed schedule for each frontrunner stations

--- app/routes/routes-items-view-model.js
+++ app/routes/routes-items-view-model.js
@@ -43,105 +43,150 @@ function RoutesItemsViewModel() {
                 station: "Ogden Station",
                 address: "2393 South Wall Avenue",
                 city: "Ogden, UT 84511",
-                times: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "Roy",
                 station: "Roy Station",
                 address: "4155 S. Sandridge Dr.",
                 city: "Roy, UT",
-                times: `6:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "Clearfield",
                 station: "Clearfield Station",
                 address: "1250 S. State St.",
                 city: "Clearfield, UT",
-                times: `4:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "Layton",
                 station: "Layton Station",
                 address: "150 S. Main St.",
                 city: "Layton, UT",
-                times: `1:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "Farmington",
                 station: "Farmington Station",
                 address: "450 N. 850 W.",
                 city: "Farmington, UT",
-                times: `3:30 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "Woods Cross",
                 station: "Wood Cross Station",
                 address: "750 S. 800 W.",
                 city: "Woods Cross, UT",
-                times: `1:30 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "North Temple",
                 station: "North Temple Station",
                 address: "500 W. North Temple",
                 city: "Salt Lake City, UT",
-                times: `2:15 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "Salt Lake Central",
                 station: "Salt Lake Central",
                 address: "250 S. 600 W.",
                 city: "Salt Lake City, UT",
-                times: `1:20 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "Murray Central Station",
                 station: "Murray Central Station",
                 address: "140 W. Vine St.",
                 city: "Murray, UT",
-                times: `2:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "South Jordan Station",
                 station: "South Jordan Station",
                 address: "10351 South Jordan Gateway",
                 city: "South Jordan, UT",
-                times: `12:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "Draper Station",
                 station: "Draper Station",
                 address: "12997 S. FrontRunner Blvd.",
                 city: "Draper, UT",
-                times: `1:40:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "Lehi Station",
                 station: "Lehi Station",
                 address: "3101 N. Ashton Blvd.",
                 city: "Lehi, UT",
-                times: `3:10 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "American Fork Station",
                 station: "American Fork Station",
                 address: "782 W. 200 S.",
                 city: "American Fork, UT",
-                times: `6:10 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "Orem Central",
                 station: "Orem Central Station",
                 address: "1350 W. 900 S.",
                 city: "Orem, UT",
-                times: `5:10 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             },
             {
                 name: "Provo Station",
                 station: "Provo Station",
                 address: "690 S. University Ave.",
                 city: "Provo, UT",
-                times: `4:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
+                northBound: "North Bound",
+                southBound: "South Bound",
+                NBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`,
+                SBtimes: `8:00 AM   9:00 AM  10:00 AM  2:00 PM   3:30 PM    4:00 PM  7:00 PM  10:00 PM 10:35 PM`
             }
         ],
         
